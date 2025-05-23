8ae6b63f17e883aa39c7abb2723c3a9c0dea4596
Viktoriya-Frank <playviktory@gmail.com>
2025-05-23 12:34:55 +0200

    Complete Spring Security

--- src/main/java/ait/cohort55/security/SecurityConfiguration.java
+++ src/main/java/ait/cohort55/security/SecurityConfiguration.java
@@ -1,16 +1,21 @@
 package ait.cohort55.security;
 
 import ait.cohort55.accounting.model.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final CustomWebSecurity webSecurity;
 
     @Bean
     SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -21,14 +26,20 @@ public class SecurityConfiguration {
                 .permitAll()
                 .requestMatchers("/account/user/{login}/role/{role}")
                 .hasRole(Role.ADMINISTRATOR.name())
-                .requestMatchers(HttpMethod.PATCH, "/account/user/{login}")
+                .requestMatchers(HttpMethod.PATCH, "/account/user/{login}", "/forum/post/{id}/comment/{login}")
                 .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
                 .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
-                .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
-                .requestMatchers(HttpMethod.POST, "/post/{author}")
+                .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+                .requestMatchers(HttpMethod.POST, "/forum/post/{author}")
                 .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
-                .requestMatchers(HttpMethod.PATCH, "/post/{id}/comment/{author}")
-                .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+                .requestMatchers(HttpMethod.PATCH, "/forum/post/{id}")
+                .access((authentication, context) -> new AuthorizationDecision(webSecurity.checkPostAuthor(context.getVariables().get("id"), authentication.get().getName())))
+                .requestMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+                .access((authentication, context) -> {
+                    boolean checkAuthor = webSecurity.checkPostAuthor(context.getVariables().get("id"), authentication.get().getName());
+                    boolean checkModerator = context.getRequest().isUserInRole(Role.MODERATOR.name());
+                    return new AuthorizationDecision(checkAuthor && checkModerator);
+                })
                 .anyRequest()
                 .authenticated());
 
